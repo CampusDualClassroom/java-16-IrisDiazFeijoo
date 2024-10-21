6ceca0a917541cdc9333ab704637cdcb47f0f870
Iris Diaz Feijoo <irisdf_4@hotmail.com>
2024-10-21 17:43:19 +0200

    Terminado ejercicio 16

--- src/main/java/com/campusdual/classroom/Exercise16.java
+++ src/main/java/com/campusdual/classroom/Exercise16.java
@@ -12,6 +12,12 @@ public class Exercise16 {
 		plane.fly();
 		tractor.forward();
 
+		tractor.stop();
+		plane.stop();
+
+		tractor.maintenance();
+		plane.maintenance();
+
 	}
 
 }
--- src/main/java/com/campusdual/classroom/IMachine.java
+++ src/main/java/com/campusdual/classroom/IMachine.java
@@ -7,3 +7,6 @@ public interface IMachine {
 	public void maintenance();
 	
 }
+
+
+
